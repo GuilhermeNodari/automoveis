0079b090fdbeb94b79231399a21b91b1fda31fa6
GuilhermeNodari <guilherme.nodari@bling.com.br>
2020-04-02 14:47:53 -0300

    X no popover

--- js/script_lista.js
+++ js/script_lista.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
     function popover() {
         adicionarPopover('popoverDescricao','A descrição serve para descrever o carro, com seu nome e motorização por exemplo');
-        adicionarPopover('popoverPlaca','Digite a placa do seu carro. Exemplo: XXX1234');
+        adicionarPopover('popoverPlaca','Digite a placa do seu carro. <br> Exemplo: XXX1234');
         adicionarPopover('popoverRenavan',' O Código RENAVAM é, hoje, um código numeral de 11 dígitos e está localizado na parte superior de sua CNH');
-        adicionarPopover('popoverAnoModelo','Digite o ano do modelo do seu carro. Exemplo: 2020');
-        adicionarPopover('popoverAnoFabricacao','Digite o ano de fabricação do seu carro. Exemplo: 2020');
+        adicionarPopover('popoverAnoModelo','Digite o ano do modelo do seu carro. <br> Exemplo: 2020');
+        adicionarPopover('popoverAnoFabricacao','Digite o ano de fabricação do seu carro. <br> Exemplo: 2020');
         adicionarPopover('popoverCor','Digite a cor do seu carro. <br> Exemplo: Branco');
         adicionarPopover('popoverKM','Digite a quilometragem do seu carro, está localizada no painel do seu automóvel');
         adicionarPopover('popoverMarca','Digite a marca do seu carro. <br> Exemplo: Fiat');
         adicionarPopover('popoverPreco','Digite o preço que você quer ganhar do seu carro');
-        adicionarPopover('popoverPrecoFipe','Digite o preço do seu carro que está na Tabela FIPE, que é a principal referência no mercado de carros usados e seminovos, além de ser usada como base para contratos e seguros. <br> Entre no site: <a href="https://veiculos.fipe.org.br/"> Veículos FIPE </a>');
+        adicionarPopover('popoverPrecoFipe','Digite o preço do seu carro que está na Tabela FIPE, que é a principal referência no mercado de carros usados e seminovos, além de ser usada como base para contratos e seguros. <br> Entre no site: <a href="https://veiculos.fipe.org.br/" target="_blank"> Veículos FIPE </a>');
     }
 
     routie({
@@ -37,16 +37,20 @@ $(document).ready(function(){
     });
 });
 
+function esconderPopover(elemento) {
+    $.each($(elemento).popover(), function(key, value) {
+        $('#'+value.id).popover('hide');
+    })
+}
+
 function adicionarPopover(id, texto) {
     $('#'+id).attr({
         'data-toggle': 'popover',
         'data-html': 'true',
         'data-placement': 'right',
-        'data-content': '<div>'+ texto + '</div>'
+        'data-content': '<a href="#cadastro" style="float:right;" onClick="Javascript:esconderPopover('+id+')"> <i class="far fa-times-circle"></i> </a> <div>'+ texto + '</div>' 
     });
-    $(document).ready(function(){
-        $('[data-toggle="popover"]').popover();
-    });
+    $('#'+id).popover();
 }
 
 function editarCadastro(id) {
